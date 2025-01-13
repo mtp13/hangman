9173c4b35a41fcd496462c21393f04f4c0d33aeb
Mike Pullen <mike.pullen@gmail.com>
2025-01-13 12:35:55 -0600

    use single quotes

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
-import { words } from "./words.js";
+import { words } from './words.js';
 const MAX_LENGTH = 4;
-const guesses = document.querySelector(".guesses");
+const guesses = document.querySelector('.guesses');
 const filteredWords = words.filter((word) => word.length <= MAX_LENGTH);
 let word = filteredWords[Math.floor(Math.random() * filteredWords.length)];
 console.log(word);
-const newGameButton = document.getElementById("new-game-button");
-let hangman = "HANGMAN".split("");
+const newGameButton = document.getElementById('new-game-button');
+let hangman = 'HANGMAN'.split('');
 const MAX_GUESSES = hangman.length;
 let numberOfIncorrectGuesses = 0;
 let gameOver = false;
 
 function initializeLetters(word) {
-  const letters = word.split("");
-  const letterGrid = document.querySelector(".letter-grid");
-  letterGrid.innerHTML = "";
+  const letters = word.split('');
+  const letterGrid = document.querySelector('.letter-grid');
+  letterGrid.innerHTML = '';
   letters.map((letter) => {
-    const letterElement = document.createElement("div");
-    letterElement.classList.add("letter");
-    letterElement.classList.add("hidden");
+    const letterElement = document.createElement('div');
+    letterElement.classList.add('letter');
+    letterElement.classList.add('hidden');
     letterElement.textContent = letter;
     letterGrid.appendChild(letterElement);
   });
 }
 
-newGameButton.addEventListener("click", newGame);
+newGameButton.addEventListener('click', newGame);
 
 function newGame() {
   word = filteredWords[Math.floor(Math.random() * filteredWords.length)];
   console.log(word);
-  hangman = "HANGMAN".split("");
+  hangman = 'HANGMAN'.split('');
   numberOfIncorrectGuesses = 0;
   gameOver = false;
-  guesses.textContent = "Try to spell the word";
+  guesses.textContent = 'Try to spell the word';
   initializeLetters(word);
   initializeKeyboard();
 }
 
 function initializeKeyboard() {
-  const keyboard = document.querySelector(".keyboard");
-  keyboard.innerHTML = "";
+  const keyboard = document.querySelector('.keyboard');
+  keyboard.innerHTML = '';
   for (let i = 65; i <= 90; i++) {
     const keyLetter = String.fromCharCode(i);
 
-    const keyLetterElement = document.createElement("div");
-    keyLetterElement.classList.add("key");
+    const keyLetterElement = document.createElement('div');
+    keyLetterElement.classList.add('key');
     keyLetterElement.textContent = keyLetter;
     keyboard.appendChild(keyLetterElement);
-    keyLetterElement.addEventListener("click", () => {
+    keyLetterElement.addEventListener('click', () => {
       if (gameOver) {
         return;
       }
-      const letterElements = document.querySelectorAll(".letter");
+      const letterElements = document.querySelectorAll('.letter');
       let found = false;
       letterElements.forEach((letterElement) => {
         if (
           letterElement.textContent.toLowerCase() === keyLetter.toLowerCase()
         ) {
-          letterElement.classList.remove("hidden");
-          keyLetterElement.classList.add("found");
+          letterElement.classList.remove('hidden');
+          keyLetterElement.classList.add('found');
           found = true;
-          const hiddenLetters = document.querySelectorAll(".hidden");
+          const hiddenLetters = document.querySelectorAll('.hidden');
           if (hiddenLetters.length === 0) {
             gameOver = true;
-            guesses.textContent = "You won!";
+            guesses.textContent = 'You won!';
           }
         }
       });
       if (!found) {
-        keyLetterElement.classList.add("not-found");
+        keyLetterElement.classList.add('not-found');
         // const hangman = document.querySelector(".hangman");
         // hangman.classList.add("show");
         numberOfIncorrectGuesses++;
@@ -80,19 +80,19 @@ function initializeKeyboard() {
 function updateGuesses(number) {
   const remainingGuesses = MAX_GUESSES - number;
   if (number === 1) {
-    guesses.textContent = "";
+    guesses.textContent = '';
   }
-  guesses.textContent += hangman.shift() + " ";
+  guesses.textContent += hangman.shift() + ' ';
 
   if (remainingGuesses === 0) {
     gameOver = true;
-    guesses.textContent += "- You lost!";
+    guesses.textContent += '- You lost!';
   }
   if (gameOver) {
-    const hiddenLetters = document.querySelectorAll(".hidden");
+    const hiddenLetters = document.querySelectorAll('.hidden');
     hiddenLetters.forEach((letter) => {
-      letter.classList.remove("hidden");
-      letter.classList.add("game-over");
+      letter.classList.remove('hidden');
+      letter.classList.add('game-over');
     });
   }
 }
